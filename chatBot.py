ed54149178114ca38a551fe49d104226c6a8d8e9
Nisha Lad <nishalad95@users.noreply.github.com>
2019-10-17 12:18:04 +0100

    first crack at python flask app - converting to accept json payload and then can be hooked up to chat platforms via webhooks, TODO: intelligence and optimization

--- chatBot.py
+++ chatBot.py
@@ -1,14 +1,13 @@
-import random
-from lxml import html
+import os
+import sys
+import json
+from datetime import datetime
+
 import requests
+from lxml import html
+from flask import Flask, request
 
-greetings = ['hola', 'hello', 'hi', 'Hi', 'hey!','hey']
-random_greeting = random.choice(greetings)
-
-question = ['How are you?','How are you doing?']
-responses = ['Okay',"I'm fine"]
-random_response = random.choice(responses)
-
+app = Flask(__name__)
 # store the html content of the common acronymns used at CERN
 cern_page = requests.get("http://library.cern/archives/accronymes?fbclid=IwAR3p33pijvUw0iaHiLdgul789O_jqlc8dStINZRyCu4rkrFBjvR7_NDo6P4")
 lhc_page = requests.get("http://maalpu.org/lhc/LHC.abbrs.htm")
@@ -16,13 +15,103 @@ tree = html.fromstring(cern_page.content + lhc_page.content)
 
 # get all the abbreviations & store them in dictionary
 elements = tree.xpath('//td/text()')
+
+
 acronymns = {}
 for e in range(0, len(elements), 2):
     acronymns[elements[e]] = elements[e+1]
 
 keys = list(acronymns.keys())
 
-while True:
-	userInput = input(">>> ")
-	if userInput.upper() in keys:
-		print(acronymns[userInput.upper()])
+
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    phrase = find_acronym(message_text) # find the acronymn
+
+                    send_message(sender_id, phrase)
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+
+def find_acronym(message):
+    if message.upper() in keys:
+        return acronymns[message.upper()]
+    return "no phrase found :-(, please try again"
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
+    try:
+        if type(msg) is dict:
+            msg = json.dumps(msg)
+        else:
+            msg = str(msg).format(*args, **kwargs)
+        print(u"{}: {}").format(datetime.now(), msg)
+    except UnicodeEncodeError:
+        pass  # squash logging errors in case of non-ascii text
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
